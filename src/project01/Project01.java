02abff3008166c658b0945ea357b75a88415fed3
sergio <ser.g.i.o@hotmail.com>
2013-02-19 23:00:43 -0500

    Update src/project01/Project01.java
    esssss

--- src/project01/Project01.java
+++ src/project01/Project01.java
@@ -13,6 +13,7 @@ public class Project01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // modificando
         dlgAccessSystem dialog=new dlgAccessSystem(new javax.swing.JFrame(), true);
         dialog.setVisible(true);
     }
