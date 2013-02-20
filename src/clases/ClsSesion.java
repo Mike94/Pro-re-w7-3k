61d716e5e5e29082238cfdc7b63e47ecb2c45bb0
Miguel <Miguel@Miguel-PC>
2013-02-20 01:45:34 +0100

    Primer Avance

---
+++ src/clases/ClsSesion.java
@@ -0,0 +1,21 @@
+package clases;
+
+/**
+ *
+ * @author MK
+ */
+public class ClsSesion {
+    //Declaracion de Variables
+    private String user=null;
+    private String[] permisosTabla;
+    
+    //EL Constructor
+    public ClsSesion(){
+        
+    }
+    
+    //_________________________________________________________:)Soy una Bara Espaciadora
+    
+    //_________________________________________________________:)Soy una Bara Espaciadora
+    
+}
---
+++ src/project01/Project01.java
@@ -0,0 +1,19 @@
+package project01;
+
+import interfaz.dlgAccessSystem;
+
+/**
+ *
+ * @author MK
+ * @fecha 20/02/2013
+ */
+public class Project01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        dlgAccessSystem dialog=new dlgAccessSystem(new javax.swing.JFrame(), true);
+        dialog.setVisible(true);
+    }
+}
